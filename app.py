9e103ad41b41faddad6daa69e47d30b4d3dc254a
darencorp <nazariy.mandebura@whiteaster.com>
2019-03-21 13:02:41 +0100

    background index

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from flask_mako import render_template, MakoTemplates
+
+mako = MakoTemplates()
 
 app = Flask(__name__)
+mako.init_app(app)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
